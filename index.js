4a205035ad98ab29e7bea943525b90e8701f4dd2
Kuhalipaul <kuhalipaul1999@gmail.com>
2024-08-11 15:23:10 +0530

    git second commit

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ require('dotenv').config()
 const express=require('express') //common js
 //import express from "express" //module js
 const app =express()
+
 const port=4000
+
+const githubData={
+hi:"kuhali"
+}
 
 app.get('/',(req,ress)=>{
     ress.send('Hello World!') 
@@ -18,6 +23,11 @@ app.get('/login',(req,res)=>{
 app.get('/youtube',(req,res)=>{
     res.send('<h2>chai aur code</h2>')
 })
+
+app.get('/github',(req,res)=>{
+    res.json(githubData)
+})
+
 app.listen(process.env.PORT,()=>{
     console.log(`Example app listening on port ${port}`)
 })
